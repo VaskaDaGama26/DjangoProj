1a66a7d4796cc088d1faa297e7aa726a4186c195
VaskaDaGama26 <131616200+VaskaDaGama26@users.noreply.github.com>
2024-11-04 22:29:27 +0300

    Update views.py

--- news/views.py
+++ news/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from .models import Article
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username='your_username').exists():
+    User.objects.create_superuser('your_username', 'your_email@example.com', 'your_password')
+
 
 def article_list(request):
     a_list = Article.objects.all()
