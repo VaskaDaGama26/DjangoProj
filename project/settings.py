af038d8daf3a294ad283499157dd38eb77811bde
VaskaDaGama26 <131616200+VaskaDaGama26@users.noreply.github.com>
2024-11-04 21:51:30 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-4p2@bm)#d&r*laezfyiyi6!##wuoeitcouz#*hjm$*ml+v7jiq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-app-name.onrender.com']
+
 
 
 # Application definition
